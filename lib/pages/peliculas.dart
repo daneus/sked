34fb68191172a7e395f1b01580ab578b69b47d0e
Ezkrman <tony.almonacid@hotmail.com>
2023-12-17 01:06:39 -0500

    Initial implementation to fetch data from database using API completed

--- lib/pages/peliculas.dart
+++ lib/pages/peliculas.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/modelo_pelicula.dart';
 
 class Peliculas extends StatelessWidget {
   const Peliculas({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<ModeloPelicula> parseMovies(String responseBody) {
+      List<dynamic> jsonData = json.decode(responseBody);
+      return jsonData.map((json) => ModeloPelicula.fromJson(json)).toList();
+    }
+
+    Future<List<ModeloPelicula>> fetchMovies() async {
+      final response =
+          await http.get(Uri.parse('http://192.168.18.12:3000/cartelera'));
+      if (response.statusCode == 200) {
+        return parseMovies(response.body);
+      } else {
+        throw Exception('Failed to retrieve movies!');
+      }
+    }
+
     return Material(
       child: Scaffold(
         body: Container(
@@ -45,7 +63,33 @@ class Peliculas extends StatelessWidget {
               Expanded(
                   child: Container(
                 margin: const EdgeInsets.only(bottom: 30),
-                color: Colors.black,
+                color: Colors.white,
+                child: FutureBuilder<List<ModeloPelicula>>(
+                  future: fetchMovies(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text("Error XD: ${snapshot.error}");
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Text("No data available!");
+                    } else {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.all(16.0),
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black),
+                                borderRadius: BorderRadius.circular(8.0)),
+                            child: Text(snapshot.data![index].title),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
               ))
             ],
           )),
