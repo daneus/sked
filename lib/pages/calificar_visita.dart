54b9ba51458926660a3186bfe735795239b50574
Ezkrman <tony.almonacid@hotmail.com>
2023-12-25 23:45:25 -0500

    Added cached_network_image dependency

---
+++ lib/pages/calificar_visita.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalificarVisita extends StatefulWidget {
+  const CalificarVisita({super.key});
+
+  @override
+  State<CalificarVisita> createState() => _CalificarVisitaState();
+}
+
+class _CalificarVisitaState extends State<CalificarVisita> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
